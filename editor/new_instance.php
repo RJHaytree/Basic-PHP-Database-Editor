6eca8ad837b818e83bfe938482de0b437a4f13b6
merjilin <31384792+RJHaytree@users.noreply.github.com>
2018-08-28 14:52:09 +0100

    Add comments to new_instance.php

--- editor/new_instance.php
+++ editor/new_instance.php
@@ -1,3 +1,12 @@
+/*
+    In this file, the respective fields are collected from the form in index.php
+
+    Upon submitting the script, this file is executed and the information from the fields are collected using the POST
+    method. These variables are then used in the insert function, which uploads the data to MySQL.
+
+    header('location:index.php'); is then used to open the index.php page once the data is submitted to MySQL.
+*/
+
 <?php
 
 include 'info_file.php';
